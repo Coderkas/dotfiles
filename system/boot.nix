20e1b8fd235df1bccd7a372f9360327f56c23365
Coderkas <92148778+Coderkas@users.noreply.github.com>
2025-07-08 21:14:30 +0200

    grub thing didnt work

--- system/boot.nix
+++ system/boot.nix
@@ -9,7 +9,6 @@
         configurationLimit = 3;
         device = "nodev";
         theme = pkgs.catppuccin-grub;
-        extraConfig = "set video=DP-2:e";
       };
 
       efi.canTouchEfiVariables = true;
