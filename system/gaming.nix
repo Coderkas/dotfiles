a7f5dd1710cff97cf9b89a0ad4f5297a25bff298
Coderkas <92148778+Coderkas@users.noreply.github.com>
2025-10-03 05:01:27 +0200

    setting proton env globally might actually be kinda annoying

--- system/gaming.nix
+++ system/gaming.nix
@@ -57,16 +57,20 @@
   environment = {
     sessionVariables = {
       STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
-      PROTON_ENABLE_WAYLAND = "1";
-      PROTON_NO_WM_DECORATION = "1";
-      # tell at least proton-ge which one is the main monitor on wayland
-      WAYLANDDRV_PRIMARY_MONITOR = "DP-2";
-      WINE_NO_WM_DECORATION = "1";
+      # bigger shader cache size so they dont have to be processed every time
+      MESA_SHADER_CACHE_MAX_SIZE = "12G";
       # maybe fix for controller stuff?
       PROTON_PREFER_SDL_INPUT = "1";
       WINE_PREFER_SDL_INPUT = "1";
-      # bigger shader cache size so they dont have to be processed every time
-      MESA_SHADER_CACHE_MAX_SIZE = "12G";
+
+      # Experimental wayland stuff
+      /*
+        PROTON_ENABLE_WAYLAND = "1";
+        PROTON_NO_WM_DECORATION = "1";
+        WINE_NO_WM_DECORATION = "1";
+      */
+      # tell at least proton-ge which one is the main monitor on wayland
+      # WAYLANDDRV_PRIMARY_MONITOR = "DP-2";
     };
 
     systemPackages = [
