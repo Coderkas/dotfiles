159dd495892593e8e6e79ab54ea48dc469d23227
Coderkas <92148778+Coderkas@users.noreply.github.com>
2026-06-12 18:41:05 +0200

    flake update, anyrun adjustments

--- modules/applications/anyrun/default.nix
+++ modules/applications/anyrun/default.nix
@@ -8,17 +8,21 @@ let
   cfg = config.machine.anyrun;
   primaryRunner = config.machine.desktop.runner.name;
   inherit (config.machine) owner;
-  anyrun-pkg = pkgs.anyrun.overrideAttrs (oldAttrs: {
-    patches = (oldAttrs.patches or [ ]) ++ [ ./anyrun.patch ];
-  });
+  anyrun-prep = pkgs.writeShellScriptBin "anyrun-prep" /* sh */ ''
+    if [[ "$1" == "no-term" ]]; then
+      echo "systemd-run --user sh -c '$2'"
+    else
+      echo $2
+    fi
+  '';
 in
 {
   options.machine.anyrun.enable = lib.mkEnableOption "Enable anyrun and its service";
 
   config = lib.mkIf (cfg.enable || primaryRunner == "anyrun") {
     machine.desktop.runner.commands = {
-      menu = "${lib.getExe anyrun-pkg} --plugins libapplications.so --plugins libshell.so --plugins librink.so";
-      web = "${lib.getExe anyrun-pkg} --plugins libwebsearch.so --plugins libdictionary.so";
+      menu = "${lib.getExe pkgs.anyrun} --plugins libapplications.so --plugins libshell.so --plugins librink.so --plugins libactions.so";
+      web = "${lib.getExe pkgs.anyrun} --plugins libwebsearch.so --plugins libdictionary.so";
     };
 
     hjem.users.${owner}.xdg.config.files."anyrun".source = ./config;
@@ -32,13 +36,14 @@ in
       serviceConfig = {
         Type = "exec";
         Slice = "session.slice";
-        ExecStart = "${lib.getExe anyrun-pkg} daemon";
+        ExecStart = "${lib.getExe pkgs.anyrun} daemon";
         NotifyAccess = "all";
       };
     };
 
     environment.systemPackages = [
-      anyrun-pkg
+      anyrun-prep
+      pkgs.anyrun
       pkgs.anyrun-provider
     ];
   };
