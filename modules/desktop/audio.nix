2748f11b594c383ca43938fc046a0d3e92a1f38d
Coderkas <92148778+Coderkas@users.noreply.github.com>
2026-07-01 16:52:05 +0200

    adding back rtkit

--- modules/desktop/audio.nix
+++ modules/desktop/audio.nix
@@ -38,25 +38,62 @@ in
       };
     };
 
-    security.pam.loginLimits = [
-      {
-        domain = "@audio";
-        type = "-";
-        item = "rtprio";
-        value = "95";
-      }
-      {
-        domain = "@audio";
-        type = "-";
-        item = "nice";
-        value = "-19";
-      }
-      {
-        domain = "@audio";
-        type = "-";
-        item = "memlock";
-        value = "4194304";
-      }
-    ];
+    security = {
+      rtkit = {
+        enable = true;
+        args = [
+          "--scheduling-policy=FIFO"
+          "--our-realtime-priority=89"
+          "--max-realtime-priority=88"
+          "--min-nice-level=-19"
+          "--rttime-usec-max=2000000"
+          "--users-max=100"
+          "--processes-per-user-max=1000"
+          "--threads-per-user-max=10000"
+          "--actions-burst-sec=10"
+          "--actions-per-burst-max=1000"
+          "--canary-cheep-msec=30000"
+          "--canary-watchdog-msec=60000"
+        ];
+      };
+      pam.loginLimits = [
+        {
+          domain = "@audio";
+          type = "-";
+          item = "rtprio";
+          value = "95";
+        }
+        {
+          domain = "@audio";
+          type = "-";
+          item = "nice";
+          value = "-19";
+        }
+        {
+          domain = "@audio";
+          type = "-";
+          item = "memlock";
+          value = "4194304";
+        }
+        {
+          domain = "@pipewire";
+          type = "-";
+          item = "rtprio";
+          value = "95";
+        }
+        {
+          domain = "@pipewire";
+          type = "-";
+          item = "nice";
+          value = "-19";
+        }
+        {
+          domain = "@pipewire";
+          type = "-";
+          item = "memlock";
+          value = "4194304";
+        }
+      ];
+    };
   };
 }
