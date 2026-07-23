b71f2331d9ae1cbd36ce35682f2d1280c89f9e58
Coderkas <92148778+Coderkas@users.noreply.github.com>
2026-07-24 01:33:27 +0200

    servitor update

--- hosts/servitor.nix
+++ hosts/servitor.nix
@@ -27,10 +27,6 @@
     kernelParams = [ "amdgpu.dcdebugmask=0x10" ];
     initrd = {
       availableKernelModules = [ "thunderbolt" ];
-      luks.devices."luks-7a488d44-e655-44ba-8871-241df2728fe1".device =
-        "/dev/disk/by-uuid/7a488d44-e655-44ba-8871-241df2728fe1";
-      luks.devices."luks-e57d3198-6f5b-42d9-a67b-a34a65b71897".device =
-        "/dev/disk/by-uuid/e57d3198-6f5b-42d9-a67b-a34a65b71897";
     };
     extraModprobeConfig = ''
       options cfg80211 ieee80211_regdom="US"
@@ -53,21 +49,21 @@
 
   fileSystems = {
     "/" = {
-      device = "/dev/disk/by-uuid/8bb605f3-824a-40d8-a22c-4dc3d660a7af";
+      device = "/dev/disk/by-uuid/f846f738-5fb2-44e8-88b5-a877149d2e2c";
       fsType = "ext4";
     };
     "/boot" = {
-      device = "/dev/disk/by-uuid/6369-1E47";
+      device = "/dev/disk/by-uuid/A6DD-C55D";
       fsType = "vfat";
       options = [
-        "fmask=0022"
-        "dmask=0022"
+        "fmask=0077"
+        "dmask=0077"
       ];
     };
   };
 
   swapDevices = [
-    { device = "/dev/disk/by-uuid/adcbb887-8ee3-4b4c-a1d7-da7de86a1ebd"; }
+    { device = "/dev/disk/by-uuid/6c9b92a7-0a6a-492b-a728-f0507ab9a2ad"; }
   ];
 
   # This value determines the NixOS release from which the default
@@ -76,5 +72,5 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+  system.stateVersion = "26.05"; # Did you read the comment?
 }
